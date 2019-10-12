d3e0903ca3271e1981d0b13d504e3613d34fe7b9
MasanoriIwakura <hagobutler@yahoo.co.jp>
2019-10-13 00:47:48 +0900

    Fix mysql settings

--- php-laravel/bootcamp/routes/web.php
+++ php-laravel/bootcamp/routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-$router->prefix('/api')->group(function () use ($router) {
-    $router->get('/diaries', 'Api/Diary/IndexDiaryController')
+Route::prefix('api')->group(function () {
+    Route::get('diaries', 'Api\Diary\IndexDiaryController')
         ->name('api.diary.index');
 });
