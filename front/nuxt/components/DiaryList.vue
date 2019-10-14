6b4016fa12d07988e49aae7b5faa82352379b260
MasanoriIwakura <hagobutler@yahoo.co.jp>
2019-10-14 12:55:21 +0900

    Add delete action

--- front/nuxt/components/DiaryList.vue
+++ front/nuxt/components/DiaryList.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="diary-list">
+    <div class="row">
+      <di class="col" v-show="errorMessage">
+        <h2 class="nes-text is-error">{{ this.errorMessage }}</h2>
+      </di>
+      <div class="col" v-show="successMessage">
+        <h2 class="nes-text is-success">{{ this.successMessage }}</h2>
+      </div>
+    </div>
+
     <div class="row" v-show="diaries.length">
       <table class="nes-table is-borderd is-centerd">
         <thead>
@@ -9,6 +18,7 @@
             <th>BODY</th>
             <th>UPDATED AT</th>
             <th>EDIT</th>
+            <th>DELETE</th>
           </tr>
         </thead>
         <tbody v-for="diary in diaries" :key="diary.id">
@@ -20,15 +30,28 @@
             <td>
               <nuxt-link class="nes-btn is-primary" :to="`${routeName}/${diary.id}`">Edit</nuxt-link>
             </td>
+            <td>
+              <button
+                type="button"
+                class="nes-btn is-warning"
+                @click="showDeleteDialog(diary.id)"
+              >Delete</button>
+            </td>
           </tr>
         </tbody>
       </table>
     </div>
-    <div class="row" v-show="this.errorMessage">
-      <h2>
-        <font style="color:red">{{ this.errorMessage }}</font>
-      </h2>
-    </div>
+
+    <dialog class="nes-dialog is-rounded" id="dialog-rounded">
+      <form method="dialog">
+        <p class="title">Danger</p>
+        <p>Do you want to delete it?</p>
+        <menu class="dialog-menu">
+          <button class="nes-btn">Cancel</button>
+          <button class="nes-btn is-error" @click="deleteAction">Delete</button>
+        </menu>
+      </form>
+    </dialog>
   </div>
 </template>
 
@@ -40,9 +63,15 @@ import Diary from "~/interfaces/diary";
 export default class DiaryList extends Vue {
   private diaries: Diary[] = [];
   private errorMessage: string = "";
+  private successMessage: string = "";
   private routeName: string = this.$nuxt.$route.params.slug;
+  private deleteId: number = 0;
 
   public mounted(): void {
+    this.getDiaries();
+  }
+
+  private getDiaries(): void {
     this.$axios
       .$get("http://localhost:8000/api/diaries")
       .then((response: Diary[]) => {
@@ -51,6 +80,34 @@ export default class DiaryList extends Vue {
       .catch((error: any) => {
         console.error(error);
         this.errorMessage = "Error. server access failure.";
+      });
+  }
+
+  private showDeleteDialog(id: number): void {
+    this.deleteId = id;
+    const dialog = document.getElementById(
+      "dialog-rounded"
+    ) as HTMLDialogElement;
+    dialog.showModal();
+  }
+
+  private deleteAction(): void {
+    // 再検索すると再描画に時間がかかるため、先にDOMだけ削除
+    this.diaries.some((diary, index) => {
+      if (diary.id == this.deleteId) this.diaries.splice(index, 1);
+    });
+
+    this.$axios
+      .$delete(`http://localhost:8000/api/diaries/${this.deleteId}`)
+      .then(() => {
+        this.successMessage = "Delete Success.";
+      })
+      .catch((error: any) => {
+        console.error(error);
+        this.errorMessage = "Error. Delete failure.";
+      })
+      .finally(() => {
+        this.getDiaries();
       });
   }
 }
