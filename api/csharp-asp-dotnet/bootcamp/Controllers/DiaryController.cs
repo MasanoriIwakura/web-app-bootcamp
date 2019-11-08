b26a18cb4ce7381c51bb6a0a2b4ffab20cf75f4f
MasanoriIwakura <hagobutler@yahoo.co.jp>
2019-11-09 03:21:15 +0900

    Fix csharp-asp-dotnet

---
+++ api/csharp-asp-dotnet/bootcamp/Controllers/DiaryController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace bootcamp.Controllers
+{
+    [ApiController]
+    [Route("diaries")]
+    public class DiaryController : ControllerBase
+    {
+        private readonly ILogger<DiaryController> _logger;
+
+        public DiaryController(ILogger<DiaryController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Diary> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Diary
+            {
+                Id = index,
+                Title = "title" + index,
+                Body = "body" + index,
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now,
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ api/csharp-asp-dotnet/bootcamp/Diary.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace bootcamp
+{
+    public class Diary 
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
