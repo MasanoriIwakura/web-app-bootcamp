7da150c76ac932ea34abb8ab31ce4189be436c3f
MasanoriIwakura <hagobutler@yahoo.co.jp>
2020-03-02 23:30:10 +0900

    Add rake-cors

--- api/ruby-on-rails/bootcamp/config/application.rb
+++ api/ruby-on-rails/bootcamp/config/application.rb
@@ -35,5 +35,14 @@ module Bootcamp
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+                 headers: :any,
+                 methods: %i[get post options head delete put patch]
+      end
+    end
   end
 end
