4d18f0b43b56e6315925411491656a83614e1adc
MasanoriIwakura <hagobutler@yahoo.co.jp>
2020-03-08 19:41:39 +0900

    Add hot load settings

--- front/next/next.config.js
+++ front/next/next.config.js
@@ -3,5 +3,14 @@ const withCSS = require("@zeit/next-css");
 module.exports = {
   withCSS: {
     url: false
+  },
+  webpackDevMiddleware: config => {
+    config.watchOptions = {
+      aggregateTimeout: 300,
+      poll: 1000,
+      ignored: [".next", "node_modules"]
+    };
+
+    return config;
   }
 };
